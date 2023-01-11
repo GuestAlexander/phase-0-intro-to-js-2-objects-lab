cf26aa588d4141b882980d8cb6abe3824144f343
Alexander <alexanderguest@me.com>
2023-01-11 13:14:40 -0500

    phase1ers

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var employee = {
+    name: "Bubba",
+    address: "moo st",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const output = { ...employee }
+    output[key] = value
+    return output
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const output = { ...employee };
+    delete output[key]
+    return output
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
